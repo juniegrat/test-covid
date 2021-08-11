c09cbb2c7175355b35b8c7f4af7bb4762a3b3ae8
Junie Grat <39712266+juniegrat@users.noreply.github.com>
2021-08-12 00:07:06 +0200

    feat<utils>: import and export all utility functions in index

---
+++ packages/landing/src/common/utils/index.js
@@ -0,0 +1,13 @@
+import findById from './findById';
+import localeStringOptions from './localeStringOptions';
+import secondToDate from './secondToDate';
+import toBase64 from './toBase64';
+import validationSchemas from './validationSchemas';
+
+export {
+  findById,
+  localeStringOptions,
+  secondToDate,
+  toBase64,
+  validationSchemas
+};
